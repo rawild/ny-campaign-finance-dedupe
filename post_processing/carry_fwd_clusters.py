7fe43034fcf2d8377b95eb7bcae5bfef51e5663c
cw2sid <annalisa.wilde@gmail.com>
2021-10-09 19:58:13 -0400

    first step to get new uuids for records

--- post_processing/carry_fwd_clusters.py
+++ post_processing/carry_fwd_clusters.py
@@ -8,11 +8,10 @@ import psycopg2.extras
 import pandas as pd
 
 
-def upload_fixed_cluster_ids(fixed_clusters_file):
-    print('getting csv for processing...')
-    df = pd.read_csv(fixed_clusters_file, dtype={0:'int',1:'int',2:'Int64'}, encoding='latin-1')
-    df = df[['orig_cluster_id','donor_id', 'new_cluster_id']]
-    df.to_csv('fixed_clusters_for_upload.csv', index=False)
+def get_new_uuids(new_filer_ids,uuids_for_fixing):
+    print('getting new filer ids csv for processing...')
+    df = pd.read_csv(new_filer_ids, dtype={0:'int',1:'str',}, encoding='latin-1')
+    df.to_csv('new_filer_ids.csv', index=False)
     
     db_conf = dj_database_url.config()
     if not db_conf:
@@ -32,35 +31,75 @@ def upload_fixed_cluster_ids(fixed_clusters_file):
     print('creating temp table...')
 
     c.execute("CREATE TEMP TABLE tmp_d"
-             "(orig_cluster_id INTEGER, donor_id INTEGER, "
-             "new_cluster_id INTEGER) ")
+             "(new_filer_id INTEGER, old_filer_id VARCHAR(12)) ")
     conn.commit()
-    with open('fixed_clusters_for_upload.csv', 'r+') as csv_file:
+    with open('new_filer_ids.csv', 'r+') as csv_file:
         c.copy_expert("COPY tmp_d "
-        "(orig_cluster_id, donor_id, new_cluster_id) "
+        "(new_filer_id, old_filer_id) "
         "FROM STDIN CSV HEADER", csv_file)
     conn.commit()
     
-    print('updating processed_donors...')
-    c.execute("UPDATE processed_donors as p"
-            " SET cluster_id = new_cluster_id, "
-            " updated = NOW() "
-            " FROM tmp_d as t"
-            " WHERE p.donor_id = t.donor_id AND t.new_cluster_id IS NOT NULL")
-    conn.commit()
+    print('getting uuid csv for processing...')
+    df = pd.read_csv(uuids_for_fixing, dtype={0:'str',1:'str',2:'float',3:'int',4:'int'}, encoding='latin-1')
+    df.to_csv('uuids_for_fixing.csv', index=False)
     
+    for index,row in df.iterrows():
+        if index % 100 == 0:
+            print(index)
+        uuid = row['uuid']
+        uuid_parts = str(uuid).split("-")
+        if len(uuid_parts) < 4:
+            df.at[index,'bad_uuid'] = 1
+            continue
+        date = str(uuid_parts[3])
+        month = date[0:2]
+        day = date[2:4]
+        year = date[4:]
+        old_filer_id = uuid_parts[0]
+        new_filer_id_query = "SELECT new_filer_id FROM tmp_d WHERE old_filer_id = '" + old_filer_id + "'"
+        c.execute(new_filer_id_query)
+        #print("getting new_filer_id")
+        new_id_record = c.fetchone()
+        new_filer_id = new_id_record[0]
+        new_uuid_mask = str(new_filer_id)+"-"+str(uuid_parts[1])+"-%-"+year+month+day
+        #print("new uuid mask= ",new_uuid_mask)
+        new_uuids_query = "SELECT uuid FROM contributions as c,processed_donors as p "\
+            "WHERE recipient_id = '"+str(new_filer_id)+"' AND type = '"+str(uuid_parts[1])+"' " \
+            "AND date = '"+year+"-"+month+"-"+day+"'AND CAST(amount AS double precision) = "+str(row["amount"])+" "\
+            "AND c.donor_id = p.donor_id "
+        c.execute(new_uuids_query)
+        #print("getting new_uuid")
+        new_uuids_result = c.fetchall()
+        #print("length new_uuids= ", len(new_uuids_result))
+        if len(new_uuids_result) != 1:
+            new_uuids_query_1 = "SELECT uuid FROM contributions as c,processed_donors as p "\
+            "WHERE recipient_id = '"+str(new_filer_id)+"' AND type = '"+str(uuid_parts[1])+"' " \
+            "AND date = '"+year+"-"+month+"-"+day+"'AND CAST(amount AS double precision) = "+str(row["amount"])+" "\
+            "AND c.donor_id = p.donor_id AND name = '"+str(row["name"])+"'"
+            c.execute(new_uuids_query_1)
+            new_uuids_result = c.fetchall()
+            if len(new_uuids_result) != 1:
+                df.at[index,'bad_result'] = len(new_uuids_result)
+                continue
+
+        for result in new_uuids_result:
+            #print("uuid =", result[0])
+            df.at[index,'new_uuid'] = result[0]
+
+    df.to_csv('uuids_with_new_uuids.csv')
     c.close()
     conn.close()
 
-    os.remove('fixed_clusters_for_upload.csv')
-    
+    os.remove('new_filer_ids.csv')
+    os.remove('uuids_for_fixing.csv')
 
 def finish():
     print("Finished")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='upload_fixed_clusters')
-    parser.add_argument('fixed_clusters_file', help='file with fixed clusters')
+    parser = argparse.ArgumentParser(prog='carry_fwd_clusters')
+    parser.add_argument('new_filer_ids', help='file with new filer ids')
+    parser.add_argument('uuids_for_fixing', help='file of uuids to fix')
     args=parser.parse_args()
-    upload_fixed_cluster_ids(args.fixed_clusters_file)
+    get_new_uuids(args.new_filer_ids,args.uuids_for_fixing)
     finish()
